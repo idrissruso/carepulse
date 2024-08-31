ed625b288697a87f5a764b5ca85558bdfd27bde7
Idrissa <idrisabkar2018@gmail.com>
2024-08-31 16:56:09 +0300

    chore: Update layout.tsx to include ThemeProvider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
-import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">Home</h1>
-      <Button>Click me</Button>
+    <div className="flex h-screen max-h-screen ">
+      <section className="remove—scrollbar container my—auto">
+        <div className="sub—container max—w—[496px]">
+          <Image
+            width={1000}
+            height={1000}
+            src={'/assets/icons/logo-full.svg'}
+            alt="care pulse"
+            className="mb-12 h-IO w-fit"
+          />
+        </div>
+      </section>
+      <section>
+        <Image
+          src={'/assets/images/onboarding-img.png'}
+          width={1000}
+          height={1000}
+          alt="onboarding"
+          className=""
+        />
+      </section>
     </div>
   )
 }
