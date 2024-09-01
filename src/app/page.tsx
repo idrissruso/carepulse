1bef4e015d8ddbd6807c0d55396cc21bd1b42435
Idrissa <idrisabkar2018@gmail.com>
2024-09-01 17:00:27 +0300

    refactor: Improve image styling and layout in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen ">
-      <section className="remove—scrollbar container my—auto pt-[60px] pl-28">
+      <section className="remove—scrollbar container my—auto pt-[60px] pl-28 ">
         <div className="sub—container max—w—[496px]">
           <Image
             width={1000}
             height={1000}
             src={'/assets/icons/logo-full.svg'}
             alt="care pulse"
-            className="mb-12 h-IO w-fit"
+            className="mb-12 w-fit"
           />
         </div>
       </section>
